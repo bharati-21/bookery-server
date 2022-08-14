cf50c85048d219ca91fdb0b1261db9e876bbc2f9
Bharati Subramanian <bharatisharada@gmail.com>
2022-08-14 15:34:13 +0530

    refactor: connect to db, set API routes

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const routes = require("./routes");
+
+const { initializeDatabaseConnection } = require("./db/db.connect");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+initializeDatabaseConnection();
+app.listen(port, () => {
+	console.log(`Listening on port ${port}...`);
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/api", routes);
 app.get("/", (req, res) => {
 	res.send("Hello, world!");
-});
-app.listen(port, () => {
-	console.log(`Listening on posrt ${port}...`);
 });
