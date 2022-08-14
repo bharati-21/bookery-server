28ab44a58a051d5469fbadbb5b029ed47fb4de3e
Bharati Subramanian <bharatisharada@gmail.com>
2022-08-14 21:42:52 +0530

    refactor: index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const routes = require("./routes");
-
 const { initializeDatabaseConnection } = require("./db/db.connect");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 const port = process.env.PORT || 5000;
 
 initializeDatabaseConnection();
@@ -14,8 +15,6 @@ app.listen(port, () => {
 	console.log(`Listening on port ${port}...`);
 });
 
-app.use(bodyParser.json());
-app.use(cors());
 app.use("/api", routes);
 app.get("/", (req, res) => {
 	res.send("Hello, world!");
