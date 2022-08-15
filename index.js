cbd0cd235ce654b88b3db839dff50ec267033432
Bharati Subramanian <bharatisharada@gmail.com>
2022-08-15 10:56:05 +0530

    refactor: connection to database

--- index.js
+++ index.js
@@ -3,23 +3,28 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const routes = require("./routes");
-const { initializeDatabaseConnection } = require("./db/db.connect");
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(bodyParser.json());
-app.use(
-	cors({
-		credentials: true,
-	})
-);
 const port = process.env.PORT || 5000;
+const mongoURL = process.env.MONGO_URL;
 
-initializeDatabaseConnection();
-app.listen(port, () => {
-	console.log(`Listening on port ${port}...`);
-});
-
+app.use(bodyParser.json());
+app.use(cors());
 app.use("/api", routes);
+
 app.get("/", (req, res) => {
-	res.send("Hello, world!");
+	res.send("Hello World!");
 });
+
+mongoose
+	.connect(mongoURL, { dbName: "bookery-ecommerce-db" })
+	.then(() => {
+		app.listen(port, () => {
+			console.log(`Listening on port ${port}...`);
+			console.log("Connected to database successfully!");
+		});
+	})
+	.catch(() => {
+		console.log("Connection Failed!");
+	});
